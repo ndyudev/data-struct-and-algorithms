e44b3ae6ee1921044806202c5bb3f9502b40bbaa
ndyudev <chauunhatduyyit@gmail.com>
2026-03-27 10:57:27 +0700

    DSA: Bubble Sort

--- src/main/java/com/ndyudev/array/BubbleSortServiceImpl.java
+++ src/main/java/com/ndyudev/array/BubbleSortServiceImpl.java
@@ -1,23 +1,53 @@
-//package com.ndyudev.array;
-//
-//public class BubbleSortServiceImpl implements BubbleSortService{
-//    /**
-//     *
-//     * @param num1
-//     * @param num2
-//     */
-//    @Override
-//    public void swap(double num1, double num2) {
-//        num1 < num2
-//    }
-//
-//    /**
-//     *
-//     * @param arr
-//     * @return
-//     */
-//    @Override
-//    public double[] bubbleSort(double[] arr) {
-//        return new double[0];
-//    }
-//}
+package com.ndyudev.array;
+
+import java.util.Scanner;
+
+public class BubbleSortServiceImpl implements BubbleSortService {
+    /**
+     *
+     * @param arr
+     * @param i
+     * @param j
+     */
+    @Override
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    @Override
+    public int[] inputArray() {
+        int length = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập độ dài của mảng:");
+
+        length = sc.nextInt();
+        int[] arr = new int[length];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Nhập giá trị phần tử tại %s: ", i);
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    /**
+     *
+     * @param arr
+     * @return
+     */
+    @Override
+    public int[] bubbleSort(int[] arr) {
+
+        int length = arr.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+        return arr;
+    }
+}
